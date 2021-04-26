63966d6a27e436b3ecc57c710aec5215c90a5e97
Jose Canales <jose.canales@utec.edu.pe>
2021-04-26 14:28:06 -0500

    Final

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 function compute() {   
-    let principal = document.getElementById("principal").value;
-    let rate = document.getElementById("rate").value;
+
+    let principal = document.getElementById("principal");
+    let rate = document.getElementById("rate");
+
     let years = document.getElementById("years").value;
     let year = new Date().getFullYear()+parseInt(years);
-    let amount = principal*years*rate/100;
+    
+    let amount = principal.value * years * rate.value/100;
 
-    document.getElementById("result").innerHTML = "\<br\>\<br\>If you deposit "+principal +",\<br\>at \
-    an interest rate of " +rate+"%\<br\>You will receive an amount of "+amount+",\<br\> in\
-    the year "+year+".\<br\>" 
+    document.getElementById("result").innerHTML = "asdfdfssadf"+"<bold class='highlight'>"+ rate.value +"</bold>" + "asasadadas"
 
-    //Highlight numbers
-    // let inputText = document.getElementById("result");
-    // let innerHTML = inputText.innerHTML;
-    // let index = innerHTML.indexOf(text);
-    // if (index >= 0) { 
-    //  innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
-    //  inputText.innerHTML = innerHTML;
-    // }
+ 
 
+    document.getElementById("result").innerHTML = "\<br\>\<br\>If you deposit "+ "<bold class='highlight'>" + principal.value + "</bold>" + ",\<br\>at \
+    an interest rate of " +"<bold class='highlight'>"+ rate.value+"%"+"</bold>" +"\<br\>You will receive an amount of "+ "<bold class='highlight'>" +amount + "</bold>" +",\<br\> in\
+    the year "+ "<bold class='highlight'>" + year + "</bold>" +".\<br\>" 
+
+  
+
+    // document.getElementById("result").innerHTML = principal.value;
+
+    
 }
 
 function updateRate(){
